279ffba61612544db1d4abbaae121ce5f22b7e87
Pi Moll <pi1402@gmail.com>
2019-05-24 23:14:46 +0200

    details template shows every model attribute

--- recipes/views.py
+++ recipes/views.py
@@ -39,12 +39,14 @@ def index(request):
 def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     picture = Picture.objects.filter(recipe=recipe_id)[0]
-    ingredients_list = Ingredient.objects.filter(recipe=recipe_id).order_by('order_item')
-    directions_list = Direction.objects.filter(recipe=recipe_id).order_by('step')
+    tags = Tag.objects.filter(recipe=recipe_id)
+    ingredients = Ingredient.objects.filter(recipe=recipe_id).order_by('order_item')
+    directions = Direction.objects.filter(recipe=recipe_id).order_by('step')
     context = {
         'recipe': recipe,
         'picture': picture,
-        'ingredients_list': ingredients_list,
-        'directions_list': directions_list
+        'tags': tags,
+        'ingredients': ingredients,
+        'directions': directions
     }
     return render(request, 'recipes/index.html', context)
