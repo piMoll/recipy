4e16aab1ae9807e367fe4fa43f6fef9af6de4f12
Pi Moll <pi1402@gmail.com>
2024-06-19 21:52:10 +0200

    Improve search: Find postfixes in titles

--- recipes/search.py
+++ recipes/search.py
@@ -1,5 +1,7 @@
 from recipes.models import Recipe, Tag
-from recipy import settings
+from django.db.models import Q, Count
+from django.contrib.postgres.aggregates import StringAgg
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def search_recipes(search_string=None, tags=None):
@@ -8,7 +10,8 @@ def search_recipes(search_string=None, tags=None):
     # TODO: optimize? //stackoverflow.com/a/8637972/4427997
     if tags:
         for tag in Tag.objects.all():
-            if tags.get(tag.name, None) is None:  # value could be set to None, or not be set at all
+            # Value could be set to None, or not be set at all
+            if tags.get(tag.name, None) is None: 
                 continue
             
             if tags[tag.name]:
@@ -17,26 +20,37 @@ def search_recipes(search_string=None, tags=None):
                 query = query.exclude(tags=tag)
     
     if search_string:
-        db_backend = settings.DATABASES['default']['ENGINE'].split('.')[-1]
-        if db_backend == 'postgresql':
-            from django.contrib.postgres.aggregates import StringAgg
-            from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-            
-            search_v = SearchVector('title', weight='B', config='german') \
-                       + SearchVector(StringAgg('ingredient__name', delimiter=' '), weight='C', config='german') \
-                       + SearchVector(StringAgg('direction__description', delimiter=' '), weight='C', config='german')
-            
-            terms = [SearchQuery(term + ':*', search_type='raw', config='german') for term in search_string.split()]
-            search_q = terms[0]
-            for sq in terms[1:]:
-                search_q &= sq
-            
-            query = query.annotate(
-                search=search_v,
-                rank=SearchRank(search_v, search_q)
-            ).filter(search=search_q).filter(rank__gt=0).order_by('-rank')
+        # First, search for word parts in title. Full text search bellow can't
+        #  do word parts.
+        contains_parts = Q()
+        for word in search_string.split(' '):
+            contains_parts = contains_parts | Q(title__icontains=word)
         
-        else:
-            query = query.filter(title__icontains=search_string)
-    
+        contains_in_title_query = query.filter(contains_parts).annotate(rank=Count('id')).distinct()
+
+        # Now let's do full text search. This will ignore filler words 
+        #  and rank results by number of occurrences
+        # Weights are: A = 1.0, B = 0.4, C = 0.2, D = 0.1
+        search_v = SearchVector('title', weight='B', config='german') \
+                   + SearchVector(StringAgg('ingredient__name', delimiter=' '), 
+                                  weight='C', config='german') \
+                   + SearchVector(StringAgg('direction__description', delimiter=' '), 
+                                  weight='C', config='german')
+        # Limitation: Full text search can find terms if they are at the
+        #  beginning of the word (term + wildcard), but not at the end. 
+        terms = [SearchQuery(term + ':*', search_type='raw', config='german') for term in search_string.split()]
+        search_q = terms[0]
+        for sq in terms[1:]:
+            search_q |= sq
+        
+        # Create a query with ranked results, filter out any results with no finds
+        text_search_query = query.annotate(
+            search=search_v,
+            rank=SearchRank(search_v, search_q)
+        ).filter(search=search_q).filter(rank__gt=0).order_by('-rank')
+        
+        # Combine title searches (including postfix searches) and ranked 
+        # full text searches. The combined query will return distinct results.
+        query = (contains_in_title_query | text_search_query).order_by('-rank')
+
     return query
--- recipes/tests.py
+++ recipes/tests.py
@@ -13,20 +13,28 @@ class SearchTestCase(TestCase):
         self.recipePizza = Recipe(title='Pizza', portion_quantity=4)
         self.recipeTomMozz = Recipe(title='TomMozzSalat', portion_quantity=4)
         self.recipeBoerek = Recipe(title='Börek', portion_quantity=4)
+        self.recipeGurkenSalat = Recipe(title='Gurkensalat', portion_quantity=4)
         
         self.recipeTomatensuppe.save() 
         self.recipePizza.save()
         self.recipeTomMozz.save()
         self.recipeBoerek.save()
+        self.recipeGurkenSalat.save()
         
-        # Incridients
+        # Incidents
         Ingredient(recipe=self.recipeTomatensuppe, name='Knoblauch', order_item=0).save()
         Ingredient(recipe=self.recipePizza, name='Zwiebeln', order_item=0).save()
+        Ingredient(recipe=self.recipePizza, name='Suppenhuhn', order_item=1).save()
         Ingredient(recipe=self.recipeTomMozz, name='Zwiebeln', order_item=0).save()
         Ingredient(recipe=self.recipeBoerek, name='Feta', order_item=0).save()
         
         # Direction
+        Direction(recipe=self.recipeTomatensuppe, description='Suppe zubereiten, aber langsam', step=0).save()
+        Direction(recipe=self.recipeTomatensuppe, description='Suppe köchern lassen, ganz langsam', step=1).save()
         Direction(recipe=self.recipePizza, description='Küche auf den Kopf stellen', step=0).save()
+        Direction(recipe=self.recipeTomMozz, description='Vor das Haus gehen und das Walholz langsam begraben', step=0).save()
+        Direction(recipe=self.recipeBoerek, description='Die Spinat suppe zubereiten, keine Cervala machen', step=0).save()
+        Direction(recipe=self.recipeBoerek, description='Am besten keine Gurke nehmen', step=1).save()
     
     def testFindInTitle(self):
         recipes: QuerySet[Recipe] = search_recipes('Tomatensuppe')
@@ -51,10 +59,27 @@ class SearchTestCase(TestCase):
     def testIgnoreFillerWords(self):
         recipes: QuerySet[Recipe] = search_recipes('auf')
         self.assertListEqual(list(recipes), [])
-        recipes: QuerySet[Recipe] = search_recipes('in')
+        recipes: QuerySet[Recipe] = search_recipes('den')
         self.assertListEqual(list(recipes), [])
         recipes: QuerySet[Recipe] = search_recipes('vor')
         self.assertListEqual(list(recipes), [])
-        recipes: QuerySet[Recipe] = search_recipes('der')
+        recipes: QuerySet[Recipe] = search_recipes('das')
         self.assertListEqual(list(recipes), [])
     
+    def testFindPostfixInTitle(self):
+        search_string = 'salat'
+        recipes: QuerySet[Recipe] = search_recipes(search_string)
+        self.assertListEqual(list(recipes), [self.recipeTomMozz, self.recipeGurkenSalat])
+        
+    def testFindTermInTitleAsPostfixAndAsWholeWordsInOtherPlaces(self):
+        search_string = 'suppe'
+        recipes: QuerySet[Recipe] = search_recipes(search_string)
+        self.assertEqual(len(list(recipes)), len([self.recipeTomatensuppe, self.recipePizza, self.recipeBoerek]))
+        self.assertListEqual(list(recipes), [self.recipeTomatensuppe, self.recipePizza, self.recipeBoerek])
+
+    def testHigherRankWithMoreMentions(self):
+        search_string = 'langsam'
+        recipes: QuerySet[Recipe] = search_recipes(search_string)
+        self.assertListEqual(list(recipes), [self.recipeTomatensuppe, self.recipeTomMozz])
+        
+
