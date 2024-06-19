0e56bc2442e47650a2c4e82ce6a810b023f0a6ce
Pi Moll <pi1402@gmail.com>
2024-06-19 21:50:40 +0200

    Switch to postgres backend only

--- recipy/settings.dev.py
+++ recipy/settings.dev.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,13 @@ WSGI_APPLICATION = 'recipy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipy',
+        'USER': 'django',
+        'PASSWORD': '****',
+        'HOST': 'localhost',
+        'PORT': '',
+        
     }
 }
 
